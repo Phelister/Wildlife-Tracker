9abcc9d6e1bc351cb383507162aa7650e51179e8
pbogonko <pbogonko@safaricom.co.ke>
2023-07-25 21:28:54 +0300

    further changes to hbs files

--- src/main/java/org/phelister/dao/AnimalsDao.java
+++ src/main/java/org/phelister/dao/AnimalsDao.java
@@ -2,6 +2,7 @@ package org.phelister.dao;
 
 import org.phelister.config.DatabaseConfig;
 import org.phelister.models.Animals;
+import org.phelister.models.Sightings;
 import org.sql2o.Connection;
 import org.sql2o.Sql2o;
 
@@ -11,33 +12,22 @@ public class AnimalsDao {
 
         private static final Sql2o sql2o = DatabaseConfig.getDatabaseTest();
 
-        public static void create(Animals animals){
+        public static void create(Sightings sightings){
 
         }
 
-        public static List<Animals> findAllHeroesBySquadId(int squadId) {
+        public static boolean  createSighting(int squadId) {
             try (Connection connection = sql2o.open()) {
                 String queryHeroes = "SELECT * FROM heroes WHERE NOT deleted AND squad_id = :squadId;";
-                return connection.createQuery(queryHeroes)
+               connection.createQuery(queryHeroes)
                         .addParameter("squadId", squadId)
                         .executeAndFetch(Animals.class);
+               return true;
             } catch (Exception exception) {
                 System.out.println(exception.getMessage());
-                return  null;
+                return false;
             }
         }
 
-        public static void assignHeroToSquad(int heroId, int squadId){
-            try (Connection connection = sql2o.open()) {
-                String queryHeroes = "UPDATE heroes SET squad_id = :squadId WHERE id = :heroId;";
-                connection.createQuery(queryHeroes)
-                        .addParameter("squadId", squadId)
-                        .addParameter("heroId", heroId)
-                        .executeUpdate();
-            } catch (Exception exception) {
-                System.out.println(exception.getMessage());
-            }
-
-        }
 
 }
