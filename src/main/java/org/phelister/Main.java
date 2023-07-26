aae8d7018bbb1cdf39efcf6a9d429d1fa082a6ee
pbogonko <pbogonko@safaricom.co.ke>
2023-07-26 14:22:11 +0300

    changed wildlife sighting

--- src/main/java/org/phelister/Main.java
+++ src/main/java/org/phelister/Main.java
@@ -16,7 +16,7 @@ public class Main {
             return SharedUtils.render(new HashMap<>(), "wildlife-details.hbs");
         });
 
-        post("/create-wildlife", (req, res)->{
+        post("/create-sighting", (req, res)->{
             String category = req.queryParams("category");
             String id = req.queryParams("id");
             String name = req.queryParams("name");
@@ -35,16 +35,42 @@ public class Main {
 
 
 
+
+
             }catch (Exception e){
 
                 System.out.print(e.getMessage());
             }
 
-//            Strength strength = new Strength();
-//            strength.setScore(score);
-//            strength.setName(name);
-//
-//            StrengthDao.create(strength);
+            res.redirect("/");
+            return null;
+        });
+
+        post("/create-animal", (req, res)->{
+            String category = req.queryParams("category");
+            String id = req.queryParams("id");
+            String name = req.queryParams("name");
+            String health= req.queryParams("health");
+            String age=req.queryParams("age");
+            String location =req.queryParams("location");
+            String rangerName =req.queryParams("rangername");
+
+            try{
+                if(category==null || category.length()==0 || id ==null  || id.length()==0|| name==null|| name.length()==0||health==null ||health.length()==0 || age ==null || age.length()==0
+                        || location==null || location.length()==0 || rangerName==null || rangerName.length()==0 ){
+                    throw new IllegalArgumentException("invalid input all fields have to be provided");
+                }
+                int latestId= Integer.valueOf(id);
+                int latestAge= Integer.valueOf(age);
+
+
+
+
+
+            }catch (Exception e){
+
+                System.out.print(e.getMessage());
+            }
 
             res.redirect("/");
             return null;
