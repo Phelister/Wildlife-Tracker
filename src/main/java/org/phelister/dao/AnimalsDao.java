cefd31b6832ee11eec8a5e18c384f9d320e05446
pbogonko <pbogonko@safaricom.co.ke>
2023-07-26 14:10:16 +0300

    modified sightings dao

--- src/main/java/org/phelister/dao/AnimalsDao.java
+++ src/main/java/org/phelister/dao/AnimalsDao.java
@@ -1,4 +1,22 @@
 package org.phelister.dao;
 
+import org.phelister.models.Sightings;
+import org.sql2o.Connection;
+
 public class AnimalsDao {
+
+    public static boolean  createSighting(Sightings sightings) {
+        try (Connection connection = sql2o.open()) {
+            String query = "INSERT INTO sightings ( animal_id ,location, ranger_name) VALUES (:animalId,:location, :rangerName);";
+            connection.createQuery(query)
+                    .addParameter("animalId", sightings.getAnimal_id())
+                    .addParameter("location", sightings.getLocation())
+                    .addParameter("rangerName",sightings.getRangerName())
+                    .executeUpdate();
+            return true;
+        } catch (Exception exception) {
+            System.out.println(exception.getMessage());
+            return false;
+        }
+    }
 }
--- src/main/java/org/phelister/dao/SightingsDao.java
+++ src/main/java/org/phelister/dao/SightingsDao.java
@@ -9,14 +9,10 @@ import org.sql2o.Sql2o;
 public class SightingsDao {
 
         private static final Sql2o sql2o = DatabaseConfig.getDatabaseProduction();
-
-        private int id;
-        private int animal_id;
-        private String location;
-        private String ranger_name;
+        private static Connection connection = sql2o.open();
 
         public static boolean  createSighting(Sightings sightings) {
-            try (Connection connection = sql2o.open()) {
+            try {
                     String query = "INSERT INTO sightings ( animal_id ,location, ranger_name) VALUES (:animalId,:location, :rangerName);";
                     connection.createQuery(query)
                             .addParameter("animalId", sightings.getAnimal_id())
