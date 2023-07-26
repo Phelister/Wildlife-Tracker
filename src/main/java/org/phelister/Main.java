00a99729797cd31d84ca52e84f7dc81438648e38
pbogonko <pbogonko@safaricom.co.ke>
2023-07-26 14:33:03 +0300

    added changes to the hbs files

--- src/main/java/org/phelister/Main.java
+++ src/main/java/org/phelister/Main.java
@@ -17,25 +17,16 @@ public class Main {
         });
 
         post("/create-sighting", (req, res)->{
-            String category = req.queryParams("category");
             String id = req.queryParams("id");
-            String name = req.queryParams("name");
-            String health= req.queryParams("health");
-            String age=req.queryParams("age");
             String location =req.queryParams("location");
             String rangerName =req.queryParams("rangername");
 
             try{
-                if(category==null || category.length()==0 || id ==null  || id.length()==0|| name==null|| name.length()==0||health==null ||health.length()==0 || age ==null || age.length()==0
+                if( id ==null  || id.length()==0
                 || location==null || location.length()==0 || rangerName==null || rangerName.length()==0 ){
                     throw new IllegalArgumentException("invalid input all fields have to be provided");
                 }
                 int latestId= Integer.valueOf(id);
-                int latestAge= Integer.valueOf(age);
-
-
-
-
 
             }catch (Exception e){
 
